7abd8b118ae75acc642733971347c552986be319
DeadCard6 <jp715224@gmail.com>
2026-06-19 10:52:32 -0500

    Add Spring Boot project scaffold

---
+++ src/test/java/com/trycore/evmTracker/EvmTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trycore.evmTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvmTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
